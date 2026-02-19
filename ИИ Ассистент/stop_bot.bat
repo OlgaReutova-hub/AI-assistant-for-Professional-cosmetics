6fd3f8a3f0e61be18354f482010223d250c5fd83
Olga Reutova <7704544@gmail.com>
2026-02-19 16:41:05 +0300

    Delete ИИ Ассистент/stop_bot.bat

--- ИИ Ассистент/stop_bot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Остановка всех процессов Python...
-taskkill /F /IM python.exe /T 2>nul
-if %errorlevel% == 0 (
-    echo Все процессы Python остановлены
-) else (
-    echo Процессы Python не найдены или уже остановлены
-)
-pause
